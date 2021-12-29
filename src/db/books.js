1e8a42a3b0dbe60f8b5d44c7e21135baec5fec30
rsundone <pavel.serhiichuk@gmail.com>
2021-12-29 19:33:54 +0200

    [med-26] put images in array on backend

--- src/db/books.js
+++ src/db/books.js
@@ -6,7 +6,7 @@ const books = [
     gunres: ["Bussiness", "Motivation"],
     price: 1520,
     liked: "10540",
-    image: "https://images-na.ssl-images-amazon.com/images/I/91mrvQ22TRL.jpg",
+    image: ["https://images-na.ssl-images-amazon.com/images/I/91mrvQ22TRL.jpg"],
     description:
       "Our most popular and trending eBooks, updated hourly. Start reading with Kobo. Check out of top eBooks in every genre to see what other readers are buying right now. eBooks In Every Genre. 24/7 Support. Download Kobo app. Top New eBook Releases.",
   },
@@ -17,8 +17,11 @@ const books = [
     gunres: ["Documentary", "History"],
     price: 1530,
     liked: "49540",
-    image:
-      "https://images-na.ssl-images-amazon.com/images/I/41+lolL22gL._SX314_BO1,204,203,200_.jpg",
+    image: [
+        "https://images-na.ssl-images-amazon.com/images/I/41+lolL22gL._SX314_BO1,204,203,200_.jpg",
+        "https://images-na.ssl-images-amazon.com/images/I/41+lolL22gL._SX314_BO1,204,203,200_.jpg",
+        "https://images-na.ssl-images-amazon.com/images/I/41+lolL22gL._SX314_BO1,204,203,200_.jpg",
+    ],
     description:
       "From a renowned historian comes a groundbreaking narrative of humanity’s creation and evolution—a #1 international bestseller—that explores the ways in which biology and history have defined us and enhanced our understanding of what it means to be “human.”",
   },
@@ -30,8 +33,7 @@ const books = [
     gunres: ["Documentary", "History"],
     price: 1550,
     liked: "6540",
-    image:
-      "https://images-na.ssl-images-amazon.com/images/I/41ABbYw2jaL._SX321_BO1,204,203,200_.jpg",
+    image: ["https://images-na.ssl-images-amazon.com/images/I/41ABbYw2jaL._SX321_BO1,204,203,200_.jpg"],
     description:
       "Human history is full of the stories of peasants, subjects, and tribes. Yet the concept of the “citizen” is historically rare—and was among America’s most valued ideals for over two centuries. But without shock treatment, warns historian Victor Davis Hanson, American citizenship as we have known it may soon vanish.",
   },
@@ -44,8 +46,7 @@ const books = [
     gunres: ["Adventurous", "Bussiness"],
     price: 2150,
     liked: "5400",
-    image:
-      "https://images-na.ssl-images-amazon.com/images/I/51vUAyizUCS._SX331_BO1,204,203,200_.jpg",
+    image: ["https://images-na.ssl-images-amazon.com/images/I/51vUAyizUCS._SX331_BO1,204,203,200_.jpg"],
     description:
       "Before Tabitha Brown was one of the most popular personalities in the world, sharing her delicious vegan home cooking and compassionate wisdom with millions of followers across social media, she was an",
   },
@@ -57,7 +58,7 @@ const books = [
     gunres: ["Adventurous", "Bussiness"],
     price: 5150,
     liked: "50",
-    image: "https://m.media-amazon.com/images/I/5137FVHUBEL._AC_UY218_.jpg",
+    image: ["https://m.media-amazon.com/images/I/5137FVHUBEL._AC_UY218_.jpg"],
     description:
       "Before Tabitha Brown was one of the most popular personalities in the world, sharing her delicious vegan home cooking and compassionate wisdom with millions of followers across social media, she was an",
   },
@@ -69,7 +70,7 @@ const books = [
     gunres: ["Adventurous", "Bussiness"],
     price: 50,
     liked: "0",
-    image: "https://images-na.ssl-images-amazon.com/images/I/81W+pBC1JFL.jpg",
+    image: ["https://images-na.ssl-images-amazon.com/images/I/81W+pBC1JFL.jpg"],
     description:
       "Before Tabitha Brown was one of the most popular personalities in the world, sharing her delicious vegan home cooking and compassionate wisdom with millions of followers across social media, she was an",
   },
@@ -81,8 +82,7 @@ const books = [
     gunres: ["Adventurous", "Bussiness"],
     price: 1250,
     liked: "1111100",
-    image:
-      "https://images-na.ssl-images-amazon.com/images/I/51OULPcNetL._AC_SX184_.jpg",
+    image: ["https://images-na.ssl-images-amazon.com/images/I/51OULPcNetL._AC_SX184_.jpg"],
     description:
       "Before Tabitha Brown was one of the most popular personalities in the world, sharing her delicious vegan home cooking and compassionate wisdom with millions of followers across social media, she was an",
   },
@@ -94,7 +94,7 @@ const books = [
     gunres: ["Adventurous", "Bussiness"],
     price: 250,
     liked: "897005",
-    image: "https://m.media-amazon.com/images/I/91P67FPmyvL._AC_UL320_.jpg",
+    image: ["https://m.media-amazon.com/images/I/91P67FPmyvL._AC_UL320_.jpg"],
     description:
       "Before Tabitha Brown was one of the most popular personalities in the world, sharing her delicious vegan home cooking and compassionate wisdom with millions of followers across social media, she was an",
   },
@@ -106,7 +106,7 @@ const books = [
     gunres: ["Adventurous", "Bussiness"],
     price: 650,
     liked: "27",
-    image: "https://m.media-amazon.com/images/I/914BkcGCNgS._AC_UL320_.jpg",
+    image: ["https://m.media-amazon.com/images/I/914BkcGCNgS._AC_UL320_.jpg"],
     description:
       "Before Tabitha Brown was one of the most popular personalities in the world, sharing her delicious vegan home cooking and compassionate wisdom with millions of followers across social media, she was an",
   },
@@ -118,8 +118,7 @@ const books = [
     gunres: ["Adventurous", "Bussiness"],
     price: 7450,
     liked: "635",
-    image:
-      "https://images-na.ssl-images-amazon.com/images/I/619M-4xNINL._AC_UL160_SR160,160_.jpg",
+    image: ["https://images-na.ssl-images-amazon.com/images/I/619M-4xNINL._AC_UL160_SR160,160_.jpg"],
     description:
       "Before Tabitha Brown was one of the most popular personalities in the world, sharing her delicious vegan home cooking and compassionate wisdom with millions of followers across social media, she was an",
   },
