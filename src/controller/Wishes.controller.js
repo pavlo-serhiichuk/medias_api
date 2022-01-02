48590cf9c2586c4045119b3545f9add51ceadca4
rsundone <pavel.serhiichuk@gmail.com>
2022-01-02 23:36:23 +0200

    [med-11] refactored Wishes.controller.js

--- src/controller/Wishes.controller.js
+++ src/controller/Wishes.controller.js
@@ -1,42 +1,46 @@
 const bodyParser = require("body-parser");
 const jsonParser = bodyParser.json();
 
-const wishes = require("./wishes.json"); 
-const books = require("../db/books.js");
-const vouchers = require("../db/vouchers.js");
-const guitars = require("../db/guitars.js");
+let wishes = require("../db/wishes.json");
 const {writeData} = require("../utils/index.js")
 
 function init(app) {
-  app.get("/wishes", (req, res) => {
+  app.put("/wishes", jsonParser, (req, res) => {
+    let {userId} = req.body
 
-    res.send(books);
+    let currentUserWishes = wishes.filter(wish => wish.userId === userId)
+    
+    res.status(200);
+    res.send(currentUserWishes);
+
   });
 
-  app.post("/wish", jsonParser, (req, res) => {
-    
-    const {userId, category, productId} = req.body;
-
-    let wish = wishes.find(w => w.userId === userId  && w.category === category  && w.productId === productId)
-    let isWishExist = !!wish;
+  app.post('/wish', jsonParser, (req, res) => {
     let wishesToWrite = [];
-    let message = ''
+    const {userId, category, productId} = req.body;
+    let index = wishes.findIndex(w => w.userId === userId && w.category === category && w.productId === productId)
+    let exist = index >= 0;
 
-    console.log({wish});
-      if(isWishExist) {
-        // delete
-        message = 'Wishes was deleted'
-        wishesToWrite = wishes.filter(w => w !== wish);
-      } else {
-        // add
-        message = 'Wishes was added'
-        wishesToWrite = [...wishes, {userId, category, productId}];
-      }
-        // write to file
-        writeData('./wishes.json', wishesToWrite)
+    if (exist) {
+      wishes.splice(index, 1); // deleted wish
+      wishesToWrite = wishes;
+    } else {
+      wishesToWrite = [...wishes, {userId, category, productId}];
+    }
+
+    // const message = exist
+    //     ? 'Wishes was deleted'
+    //     : 'Wishes was added'
+
+    wishes = wishesToWrite;
+    writeData('../src/db/wishes.json', wishesToWrite)
+
+    let currentUserWishes = wishes.filter(wish => wish.userId === userId)
+
+console.log(currentUserWishes);
 
     res.status(200);
-    res.send({message});
+    res.send(currentUserWishes);
   });
 }
 
