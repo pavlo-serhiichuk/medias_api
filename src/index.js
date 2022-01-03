1fa85967878a20679aab8b78f21d8e02bdd2f4bf
spavelo <61013347+spavelo@users.noreply.github.com>
2022-01-03 19:37:15 +0200

    Revert "Med 11 add filters for wishes on backend"

--- src/index.js
+++ src/index.js
@@ -9,7 +9,6 @@ const GuitarsController = require('./controller/Guitars.controller')
 const VouchersController = require('./controller/Vouchers.controller')
 const CountriesController = require('./controller/Countries.controller')
 const ArticleController = require('./controller/Article.controller')
-const WishesController = require('./controller/Wishes.controller')
 
 app.use(cors({
     origin: ['http://localhost:3000'],
@@ -28,7 +27,6 @@ GuitarsController(app);
 VouchersController(app);
 CountriesController(app);
 ArticleController(app);
-WishesController(app);
 
 app.listen(port, () => {
     console.log('App is run on: http://localhost:' + port)
--- src/utils/filters.js
+++ src/utils/filters.js
@@ -1,4 +1,3 @@
-
 function filterProducts(category, body, res) {
   let { filterName, filterParam } = body;
 
